1251f84c69826ff4a4e357aae145c7df42401664
mac <mac@Jade-MBP.local>
2019-01-16 13:31:29 +0700

    angularfire config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// angularfire setup
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment'
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +18,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCj9b4-DeF_zyn2gUuAmBEukG42XIx9SJ0",
+    authDomain: "messaging-push-sample.firebaseapp.com",
+    databaseURL: "https://messaging-push-sample.firebaseio.com",
+    projectId: "messaging-push-sample",
+    storageBucket: "messaging-push-sample.appspot.com",
+    messagingSenderId: "523826768688"
+  }
 };
 
 /*
